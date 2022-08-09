c7ef266ee52949272b9316f42308cd851159a03d
luzede <85195923+luzede@users.noreply.github.com>
2022-08-09 18:13:09 +0300

    1.6 done

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,32 +1,31 @@
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
+import { useState } from "react"
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick= {handleClick}>
+      {text}
+    </button>
+  )
+}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      },
-    ]
-  }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
 
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
