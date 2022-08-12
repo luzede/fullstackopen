f636f825e317df359132b28fbd5730752de6a3e5
luzede <85195923+luzede@users.noreply.github.com>
2022-08-12 16:24:04 +0300

    Something went wrong idk

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 import Persons from './Persons'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
-
     const [newPhoneNumber, setNewPhoneNumber] = useState('')
-
     const [filtered, setFilter] = useState('')
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/persons')
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, [])
 
     const handleInputName = (event) => {
         setNewName(event.target.value);
