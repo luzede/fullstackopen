fbc95be62f6512017cd7f6506dfc0cf24675ea7a
luzede <85195923+luzede@users.noreply.github.com>
2022-08-19 20:32:57 +0300

    Blog list of part4 done

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,44 +1,10 @@
-// const http = require('http');
-const express = require('express');
+const http = require('http');
+const app = require('./app');
+const config = require('./utils/config');
 
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const server = http.createServer(app);
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
-
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
+const PORT = config.PORT || 3003;
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ part4/blog_list/models/blog.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
+const mongoose = require('mongoose');
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+blogSchema.set('toJSON', (_document, returnedObject) => {
+  // returnedObject.id = returnedObject.id could probably be used instead
+  returnedObject.id = returnedObject._id.toString();
+  delete returnedObject._id;
+  delete returnedObject.__v;
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
