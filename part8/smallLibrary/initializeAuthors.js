25253d762b8f38fb712bcbd39278acfbd09111e4
luzede <85195923+luzede@users.noreply.github.com>
2022-10-17 00:01:57 +0300

    initialized the databse for authors and books and started changing resolvers

--- part8/smallLibrary/initializeAuthors.js
+++ part8/smallLibrary/initializeAuthors.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const Author = require('./schemas/author')
-const Book = require('./schemas/book')
 const { MONGODB_URI } = require('./config')
 
 const authors = [
@@ -28,57 +27,6 @@ const authors = [
     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
   },
 ]
-const books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
 
 
 mongoose.connect(MONGODB_URI)
@@ -89,7 +37,7 @@ mongoose.connect(MONGODB_URI)
     console.log(`Failed to connect to MongoDB\n--------\n${error.message}`);
   })
 
-//Delete documents in a collection of authors and books
+// Delete documents in a collection of authors
 Author.deleteMany({})
   .then(() => {
     console.log('Successfully deleted documents in "authors"');
---
+++ part8/smallLibrary/initializeBooks.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose')
+const Author = require('./schemas/author')
+const Book = require('./schemas/book')
+const { MONGODB_URI } = require('./config')
+
+const books = [
+  {
+    title: 'Clean Code',
+    published: 2008,
+    author: 'Robert Martin',
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+  },
+  {
+    title: 'Agile software development',
+    published: 2002,
+    author: 'Robert Martin',
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ['agile', 'patterns', 'design']
+  },
+  {
+    title: 'Refactoring, edition 2',
+    published: 2018,
+    author: 'Martin Fowler',
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+  },
+  {
+    title: 'Refactoring to patterns',
+    published: 2008,
+    author: 'Joshua Kerievsky',
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring', 'patterns']
+  },
+  {
+    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+    published: 2012,
+    author: 'Sandi Metz',
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring', 'design']
+  },
+  {
+    title: 'Crime and punishment',
+    published: 1866,
+    author: 'Fyodor Dostoevsky',
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ['classic', 'crime']
+  },
+  {
+    title: 'The Demon ',
+    published: 1872,
+    author: 'Fyodor Dostoevsky',
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ['classic', 'revolution']
+  },
+]
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log(`Failed to connect to MongoDB\n--------\n${error.message}`);
+  });
+
+(async function () {
+  await Book.deleteMany({})
+  console.log('Successfully delete documents in "books"');
+  
+  const booksWithAuthorId = await Promise.all(books.map(async (b) => {
+    const author = await Author.findOne({ name: b.author })
+    if (!author) return null
+    return {
+      title: b.title,
+      author: mongoose.Types.ObjectId(author._id.toString()),
+      published: b.published,
+      genres: b.genres
+    }
+  }))
+
+  // console.log(booksWithAuthorId);
+
+  await Book.insertMany(booksWithAuthorId)
+  console.log('Successfully initialized "books"');
+
+  await mongoose.connection.close()
+})();
