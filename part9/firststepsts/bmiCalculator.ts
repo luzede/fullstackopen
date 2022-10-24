dfc57259da42a686da4f12e1b74b5c676d28fc0d
luzede <85195923+luzede@users.noreply.github.com>
2022-10-24 16:38:00 +0300

    9.1 done

---
+++ part9/firststepsts/bmiCalculator.ts
@@ -0,0 +1,21 @@
+
+function calculateBmi(height: number, weight: number): string {
+  const bmi = weight * 10000 / (height * height)
+  if (bmi <= 18.5) {
+    return "Underweight (unhealthy weight)"
+  }
+  else if (bmi < 25) {
+    return "Normal (healthy weight)"
+  }
+  else if (bmi < 30) {
+    return "Overweight (unhealthy weight)"
+  }
+  else if (bmi >= 30) {
+    return "Obese (very unhealthy weight)"
+  }
+  else {
+    throw new Error("Invalid input")
+  }
+}
+
+console.log(calculateBmi(180, 74))
