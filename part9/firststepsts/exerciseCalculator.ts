ae6fe6cf2452688d01060874f4113bc8c0ac3106
luzede <85195923+luzede@users.noreply.github.com>
2022-10-24 22:44:45 +0300

    9.2 done

---
+++ part9/firststepsts/exerciseCalculator.ts
@@ -0,0 +1,53 @@
+
+interface Result {
+  periodLength: number,
+  trainingDays: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string,
+  target: number,
+  average: number
+}
+
+
+const calculateExercises = (array: Array<number>, target: number): Result => {
+  let total: number = 0
+  let trainingDays: number = 0
+  
+  
+  
+  
+
+  const periodLength: number = array.length
+
+
+  array.forEach((item: number) => {
+    total += item
+    if (item > 0) trainingDays++
+  })
+
+  const average: number = total/periodLength
+  const success: boolean = average > target
+  const rating: number = (average < target) ? 1 : (average - target) >= 1 ? 3 : 2
+  let ratingDescription: string = (rating === 1)
+    ? "You did poorly"
+    : (rating === 2)
+    ? "You did good"
+    : (rating === 3)
+    ? "You did great"
+    : "There was an error"
+  
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+
