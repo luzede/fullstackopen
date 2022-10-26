5681b037ba5ed7a4a497fd7df5169e1c8f35b4d0
luzede <85195923+luzede@users.noreply.github.com>
2022-10-26 19:58:02 +0300

    9.5 done

--- part9/firststepsts/index.ts
+++ part9/firststepsts/index.ts
@@ -10,11 +10,42 @@ app.get('/hello', (_req, res) => {
   res.send('Hello Fullstack!')
 })
 
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height)
+  const weight = Number(req.query.weight)
+  
+  res.json({
+    height,
+    weight,
+    bmi: calculateBmi(height, weight)
+  })
+})
+
 const PORT = 3003;
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT: ${PORT}`);
 })
+
+
+function calculateBmi(height: number, weight: number): string {
+  const bmi = weight * 10000 / (height * height)
+  if (bmi <= 18.5) {
+    return "Underweight (unhealthy weight)"
+  }
+  else if (bmi < 25) {
+    return "Normal (healthy weight)"
+  }
+  else if (bmi < 30) {
+    return "Overweight (unhealthy weight)"
+  }
+  else if (bmi >= 30) {
+    return "Obese (very unhealthy weight)"
+  }
+  else {
+    throw new Error("Invalid input")
+  }
+}
 
 
 
