4fb1a4e90a751a6cb8c8440406da864c7ba4eb7a
luzede <85195923+luzede@users.noreply.github.com>
2022-10-27 12:54:56 +0300

    9.8 done

--- part9/firststepsts/index.ts
+++ part9/firststepsts/index.ts
@@ -30,16 +30,18 @@ app.get('/bmi', (req, res) => {
 });
 
 app.post('/exercises', (req, res) => {
-  
-  const { daily_exercises, target } = req.body
+  // eslint-disable-next-line
+  const { daily_exercises, target } = req.body;
+
   try {
-    res.status(200).json(calculateExercises(daily_exercises, target))
+    // eslint-disable-next-line
+    res.status(200).json(calculateExercises(daily_exercises, target));
   } catch {
     res.json({
       error: "malformatted parameters"
-    })
+    });
   }
-})
+});
 
 const PORT = 3003;
 
