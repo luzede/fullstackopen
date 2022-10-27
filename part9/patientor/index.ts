5e888dc171f9c0609b560519d0b0af4091819ec4
luzede <85195923+luzede@users.noreply.github.com>
2022-10-27 15:21:16 +0300

    9.11 done

--- part9/patientor/index.ts
+++ part9/patientor/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import diagnosesRouter from './src/routes/diagnoses';
+import patientsRouter from './src/routes/patients';
 
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientsRouter);
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
