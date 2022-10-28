6d2c3ca42693e5f8ea21fce425325645ec5aa673
luzede <85195923+luzede@users.noreply.github.com>
2022-10-28 20:15:14 +0300

    9.15 done

--- part9/courseinfo/src/App.tsx
+++ part9/courseinfo/src/App.tsx
@@ -4,20 +4,40 @@ import Total from "./components/Total";
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the easy course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the hard course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
-  ];
+  ]
 
   return (
     <div>
@@ -29,3 +49,33 @@ const App = () => {
 };
 
 export default App;
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CourseNormalPart extends CoursePartBase {
+  type: "normal";
+  description: string;
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: "submission";
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CoursePartBase {
+  type: "special",
+  description: string,
+  requirements: string[]
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
--- part9/courseinfo/src/components/Content.tsx
+++ part9/courseinfo/src/components/Content.tsx
@@ -1,21 +1,82 @@
 
-interface CoursePart {
-  name: string,
-  exerciseCount: number
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
 }
+
+interface CourseNormalPart extends CoursePartBase {
+  type: "normal";
+  description: string;
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBase {
+  type: "submission";
+  description: string;
+  exerciseSubmissionLink: string;
+}
+
+interface CourseSpecialPart extends CoursePartBase {
+  type: "special",
+  description: string,
+  requirements: string[]
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
 
 const Content = ({courseParts}: {courseParts: CoursePart[]}): JSX.Element => {
   return (
     <div>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
+      {
+        courseParts.map(c => {
+          switch(c.type) {
+            case "normal":
+              return (
+                <div key={c.name}>
+                  <h2>{c.name}</h2>
+                  <p>{c.description}</p>
+                  <p>Exercise count: {c.exerciseCount}</p>
+                </div>
+              )
+            case "groupProject":
+              return (
+                <div key={c.name}>
+                  <h2>{c.name}</h2>
+                  <p>Group project count: {c.groupProjectCount}</p>
+                  <p>Exercise count: {c.exerciseCount}</p>
+                </div>
+              )
+            case "submission":
+              return (
+                <div key={c.name}>
+                  <h2>{c.name}</h2>
+                  <p>{c.description}</p>
+                  <p>{c.exerciseSubmissionLink}</p>
+                  <p>Exercise count: {c.exerciseCount}</p>
+                </div>
+              )
+            case "special":
+              return (
+                <div key={c.name}>
+                  <h2>{c.name}</h2>
+                  <p>{c.description}</p>
+                  <p>Requirements: {c.requirements.reduce((a,b) => a + ' / ' + b)}</p>
+                  <p>Exercise count: {c.exerciseCount}</p>
+                </div>
+              )
+            default:
+              return (function assertNever(value: never): never {
+                throw new Error(`Unhandled discriminated union member ${JSON.stringify(value)}`)
+              })(c)
+          }
+        })
+      }
     </div>
     )
 }
