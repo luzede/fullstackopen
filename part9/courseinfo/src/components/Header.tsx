6865bd2ef5a4200fd22c26e01336156c369cf935
luzede <85195923+luzede@users.noreply.github.com>
2022-10-28 14:27:16 +0300

    9.14 done

---
+++ part9/courseinfo/src/components/Header.tsx
@@ -0,0 +1,8 @@
+
+const Header = ({courseName}: {courseName: string}): JSX.Element => {
+  return (
+    <h1>{courseName}</h1>
+  )
+}
+
+export default Header
---
+++ part9/courseinfo/src/components/Total.tsx
@@ -0,0 +1,18 @@
+
+interface CoursePart {
+  name: string,
+  exerciseCount: number
+}
+
+const Total = ({ courseParts }: {courseParts: CoursePart[]}): JSX.Element => {
+  return (
+    <div>
+      <p>
+        Number of exercises{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+    </div>
+  )
+}
+
+export default Total
